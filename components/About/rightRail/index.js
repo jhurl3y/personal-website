c7567849ea4376bf5a3895e38654f520b2b41052
James Hurley <jhurley070@gmail.com>
2024-06-02 20:11:00 -0700

    styles about

--- components/About/rightRail/index.js
+++ components/About/rightRail/index.js
@@ -5,42 +5,64 @@ import Grid from "@mui/material/Grid";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { aboutStrings } from "../../../utils/strings";
 import { getAge } from "../../../utils/helpers";
-import Styles from "./styles";
 
-const Icon = ({ icon, text, classes }) => (
+const Icon = ({ icon, text }) => (
   <div>
     <FontAwesomeIcon icon={icon} size="2x" className="highlight" />
-    <p className={classes.skillText}>{text}</p>
+    <p sx={{ paddingRight: (theme) => theme.spacing(4) }}>{text}</p>
   </div>
 );
 
 export default () => {
-  const classes = Styles();
   const { me, intro, iLike, code, sport, travel, friends } = aboutStrings;
 
   return (
-    <Grid container direction="column" className={classes.rightRail}>
+    <Grid
+      container
+      direction="column"
+      sx={{ padding: (theme) => theme.spacing(6, 6, 6, 8) }}
+    >
       <Grid item xs={12}>
-        <Container maxWidth={false} className={classes.rightRailContent}>
+        <Container
+          maxWidth={false}
+          sx={(theme) => ({
+            paddingBottom: (theme) => theme.spacing(6),
+            [theme.breakpoints.down("xs")]: {
+              textAlign: "center",
+            },
+          })}
+        >
           <Typography variant="h4">{me}</Typography>
           <p>{intro.replace("{age}", getAge("1994/07/14"))}</p>
         </Container>
       </Grid>
       <Grid item xs={12}>
-        <Container maxWidth={false} className={classes.rightRailContent}>
+        <Container
+          maxWidth={false}
+          sx={(theme) => ({
+            paddingBottom: (theme) => theme.spacing(6),
+            [theme.breakpoints.down("xs")]: {
+              textAlign: "center",
+            },
+          })}
+        >
           <Typography variant="h4">{iLike}</Typography>
-          <Grid container spacing={2} className={classes.icons}>
+          <Grid
+            container
+            spacing={2}
+            sx={{ padding: (theme) => theme.spacing(6, 0) }}
+          >
             <Grid item xs={12} sm={6}>
-              <Icon icon="code" text={code} classes={classes} />
+              <Icon icon="code" text={code} />
             </Grid>
             <Grid item xs={12} sm={6}>
-              <Icon icon="heartbeat" text={sport} classes={classes} />
+              <Icon icon="heartbeat" text={sport} />
             </Grid>
             <Grid item xs={12} sm={6}>
-              <Icon icon="train" text={travel} classes={classes} />
+              <Icon icon="train" text={travel} />
             </Grid>
             <Grid item xs={12} sm={6}>
-              <Icon icon="users" text={friends} classes={classes} />
+              <Icon icon="users" text={friends} />
             </Grid>
           </Grid>
         </Container>
