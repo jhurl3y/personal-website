0e3e859cf74fb8840c0c2b1bf0921e9f68a404a1
James Hurley <jhurley070@gmail.com>
2024-06-02 22:40:41 -0700

    more cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -53,6 +53,42 @@ export default function MyApp({ Component, pageProps }) {
           href="/static/favicon_io/favicon-16x16.png"
         />
         <link rel="manifest" href="/static/favicon_io/site.webmanifest" />
+        <meta charSet="utf-8" />
+        <meta name="title" content={metaStrings.title} />
+        <meta name="description" content={metaStrings.description} />
+        <meta name="keywords" content={metaStrings.keywords} />
+        <meta name="author" content={metaStrings.author} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="canonical" href={metaStrings.url}></link>
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content={metaStrings.twitterSite} />
+        <meta name="twitter:title" content={metaStrings.title} />
+        <meta name="twitter:description" content={metaStrings.description} />
+        <meta
+          name="twitter:image:src"
+          content={`${metaStrings.url}/static/assets/images/about_meta.png`}
+        />
+        <meta
+          name="twitter:image"
+          content={`${metaStrings.url}/static/assets/images/about_meta.png`}
+        />
+        <meta name="twitter:image:width" content="300" />
+        <meta name="twitter:image:height" content="349" />
+        {/* Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={metaStrings.title} />
+        <meta property="og:site_name" content={metaStrings.title} />
+        <meta property="og:description" content={metaStrings.description} />
+        <meta
+          property="og:image"
+          content={`${metaStrings.url}/static/assets/images/about_meta.png`}
+        />
+        <meta property="og:image:width" content="300" />
+        <meta property="og:image:height" content="349" />
+        <meta property="og:locale" content="en_GB" />
+        <meta property="og:url" content={metaStrings.url}></meta>
       </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
--- pages/_document.js
+++ pages/_document.js
@@ -1,76 +1,19 @@
 import React from "react";
 import Document, { Head, Main, NextScript, Html } from "next/document";
 import { ServerStyleSheets } from "@mui/styles";
-import { metaStrings } from "../utils/strings";
-import cssnano from "cssnano";
-import postcss from "postcss";
-import autoprefixer from "autoprefixer";
-
-const prefixer = postcss([autoprefixer]);
-const minifier = postcss([cssnano]);
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <meta charSet="utf-8" />
-          <meta name="title" content={metaStrings.title} />
-          <meta name="description" content={metaStrings.description} />
-          <meta name="keywords" content={metaStrings.keywords} />
-          <meta name="author" content={metaStrings.author} />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="canonical" href={metaStrings.url}></link>
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
-          {/* Twitter */}
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content={metaStrings.twitterSite} />
-          <meta name="twitter:title" content={metaStrings.title} />
-          <meta name="twitter:description" content={metaStrings.description} />
-          <meta
-            name="twitter:image:src"
-            content={`${metaStrings.url}/static/assets/images/about_meta.png`}
-          />
-          <meta
-            name="twitter:image"
-            content={`${metaStrings.url}/static/assets/images/about_meta.png`}
-          />
-          <meta name="twitter:image:width" content="300" />
-          <meta name="twitter:image:height" content="349" />
-          {/* Facebook */}
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content={metaStrings.title} />
-          <meta property="og:site_name" content={metaStrings.title} />
-          <meta property="og:description" content={metaStrings.description} />
-          <meta
-            property="og:image"
-            content={`${metaStrings.url}/static/assets/images/about_meta.png`}
-          />
-          <meta property="og:image:width" content="300" />
-          <meta property="og:image:height" content="349" />
-          <meta property="og:locale" content="en_GB" />
-          <meta property="og:url" content={metaStrings.url}></meta>
         </Head>
         <body>
           <Main />
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.GA_ID}');
-              `,
-            }}
-          />
           <NextScript />
         </body>
       </Html>
@@ -112,17 +55,12 @@ MyDocument.getInitialProps = async (ctx) => {
 
   const initialProps = await Document.getInitialProps(ctx);
   const css = sheets.toString();
-  const prefixedCss = await prefixer.process(css);
-  const minifiedCss = await minifier.process(prefixedCss.css);
 
   return {
     ...initialProps,
     // Styles fragment is rendered after the app and page rendering finish.
     styles: (
-      <style
-        id="jss-server-side"
-        dangerouslySetInnerHTML={{ __html: minifiedCss.css }}
-      />
+      <style id="jss-server-side" dangerouslySetInnerHTML={{ __html: css }} />
     ),
   };
 };
